9a06d3d6b1c0ae80745139e16420fc6c97b702c7
NIGHT <3210103847@zju.edu.cn>
2024-08-24 23:16:09 +0800

    。

--- week2/SLList.java
+++ week2/SLList.java
@@ -1,58 +1,58 @@
 /** An SLList is a list of integers, which hides the terrible truth of the nakedness within */
 /** IntNode actually 对链表节点结构进行了一个封装 */
 public class SLList {
-    private IntNode first;
+    // 设定一个固定的第一个节点，这样就不会出现链表首尾衔接的情况 The first item (if it exists) is at sentinel.next
+    private IntNode sentinel;
     // make it private, so that the user can't access it from outside class
     // private 就是更抽象底层的东西，通过设置private，user就不能直接访问这个变量，从而防止链表首尾衔接的情况。对于外部用户来说，they could never make IntNode
+    private int size; // create size to store the size of the list
 
+    // The function is for creating an empty SLList
+    public SLList(){
+        sentinel = new IntNode(5, null);
+        size = 0;
+    }
+
+    // The function is for creating an SLList with an initial value
     public SLList(int x){
-        first = new IntNode(x, null);
+        sentinel = new IntNode(5, null);
+        sentinel.next = new IntNode(x, null);
+        size = 1;
     }
-    public static void main(String[] args){
-        SLList L = new SLList(15);
-        L.addFirst(5);
-        L.addFirst(10);
-        L.addLast(20); 
-        System.out.println(L.getFirst());
-    }
+    
 
     /** add x to the front of the list */
     /** IntNode actually 对链表节点结构进行了一个封装 */
     public void addFirst(int x){
-        first = new IntNode(x, first);
+        sentinel.next = new IntNode(x, sentinel.next); // 这里的sentinel.next就是原来的first
+        size += 1;
     }
 
     /** return the first item in the list */
     public int getFirst(){
-        return first.item;
-    }
-
-    public SLList(){
-        // create an empty SLList
-        first = null;
+        return sentinel.next.item;
     }
 
     public void addLast(int x){
-        IntNode p = first;
+        IntNode p = sentinel;
         // scan p to the end of the list
         while(p.next != null){
             p = p.next;
         }
         p.next = new IntNode(x, null);
-    }
-
-    /** return the size starts from p */
-    private static int size(IntNode p){
-        if(p.next == null){
-            return 1;
-        }
-        return 1 + size(p.next);
+        size += 1;
     }
 
     // public method speaks the language of people, private method speaks the language of gods
     public int size(){
-        return size(first);
+        return size;
     }
     // 要让size更快，简化size的计算，可以从一开始就设置一个size变量，每次add都+1
+
+    public static void main(String[] args){
+        SLList L = new SLList();
+        L.addLast(20);
+        System.out.println(L.size());
+    }
 
 } 
